89db50e79511528c9166c54179154fd5c4cdc393
2003myriam <lounesmyriam210@gmail.com>
2026-03-21 11:52:38 +0100

    first step

---
+++ script.js
@@ -0,0 +1,8 @@
+let quantité=document.getElementsByClassName("quantity")
+console.log(quantité[0].innerText)
+let btnplus=document.getElementsByClassName("fa-plus-circle")
+let btnmoins=document.getElementsByClassName("fa-minus-circle")
+
+
+ 
+ 
