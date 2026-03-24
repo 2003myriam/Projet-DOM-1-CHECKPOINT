3ee661b1308042804b6ee385ce4c98c07bd3b360
2003myriam <lounesmyriam210@gmail.com>
2026-03-24 15:34:36 +0100

    Permettre d'ajuster la quantité des articles avec + et -

--- script.js
+++ script.js
@@ -4,5 +4,29 @@ let btnplus=document.getElementsByClassName("fa-plus-circle")
 let btnmoins=document.getElementsByClassName("fa-minus-circle")
 
 
+for (let i = 0; i < quantité.length; i++) {
+ let valeurQuantité=Number(quantité[i].innerText)
+ console.log(valeurQuantité);
+ function add() {
+  valeurQuantité=valeurQuantité+1
+  quantité[i].innerText=String(valeurQuantité)
+ }
+ btnplus[i].addEventListener("click",function(){
+  add()
+})
+ function moins() {
+  valeurQuantité=valeurQuantité-1
+  quantité[i].innerText=String(valeurQuantité)
+ }
+ btnmoins[i].addEventListener("click",function(){
+  moins()
+})
+}
+
+
+
+
+
+
  
  
